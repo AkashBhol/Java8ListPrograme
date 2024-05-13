3dddff81412da7dee49aa81a8d308346e536426f
AkashBhol <154880170+AkashBhol@users.noreply.github.com>
2024-05-13 23:15:55 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -14,32 +14,32 @@ public class Test {
         li.add(new Student(2, "alok", 98600.90f));
         li.add(new Student(8, "bijay", 7800.90f));
 
-        // forEach it is internally using consumer
-        // li.stream().forEach(p->
-        // System.out.println(p)
-        // );
+        forEach it is internally using consumer
+        li.stream().forEach(p->
+        System.out.println(p)
+        );
 
         // filter
         // i want all the records whose sal is morethen 8000
-        // li= li.stream().filter(p->(double) p.getSal()>=8000).toList();
-        // li.forEach(System.out::println);//::->method reference
+        li= li.stream().filter(p->(double) p.getSal()>=8000).toList();
+        li.forEach(System.out::println);//::->method reference
 
         // sorted
         // based on id
-        // li=li.stream().sorted((e1,e2)->(int) e1.getId()-e2.getId()).toList();
-        // li.stream().forEach(p->System.out.println(p));
+        li=li.stream().sorted((e1,e2)->(int) e1.getId()-e2.getId()).toList();
+        li.stream().forEach(p->System.out.println(p));
 
         // sorted based on sal
         // on doubl value
-        // li=li.stream().sorted((e1,e2)->
-        // Float.compare(e1.getSal(),e2.getSal())).toList();
-        // li.stream().forEach(p->System.out.println(p));
+        li=li.stream().sorted((e1,e2)->
+        Float.compare(e1.getSal(),e2.getSal())).toList();
+        li.stream().forEach(p->System.out.println(p));
 
         // sort and find the secnd heightest sal
-        // li=li.stream().sorted((e1,e2)->Float.compare(e2.getSal(),
-        // e1.getSal())).skip(1).limit(1).toList();
-        // li.stream().forEach(p->System.out.println(p));
-        // ArrayList<String> li2=new ArrayList<>();
+        li=li.stream().sorted((e1,e2)->Float.compare(e2.getSal(),
+        e1.getSal())).skip(1).limit(1).toList();
+        li.stream().forEach(p->System.out.println(p));
+        ArrayList<String> li2=new ArrayList<>();
         // li.stream().map(p->p.getName()).forEach(p->System.out.println(p));
 
         // find the sal in range
